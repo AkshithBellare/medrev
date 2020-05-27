ab2bbd360e4a22c1e718c2f6cac7052039d9b5bd
AkshithBellare <akshithnm@gmail.com>
2020-05-28 00:07:27 +0530

    wrote actions to get comments and get drug data

--- frontend/src/store/actions.type.js
+++ frontend/src/store/actions.type.js
@@ -3,3 +3,5 @@ export const LOGOUT = "logout";
 export const REGISTER = "register";
 export const CHECK_AUTH = "checkAuth";
 export const SEARCH_DRUGS = "searchDrugs";
+export const GET_DRUG = "getDrug";
+export const GET_DRUG_COMMENTS = "getDrugComments"
